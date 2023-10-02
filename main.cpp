5696c3826afe108c6b004df9a02a00cec5845d4b
gaurav1954 <500090993@stu.upes.ac.in>
2023-10-02 09:20:55 +0530

    binary serach added

--- main.cpp
+++ main.cpp
@@ -11,11 +11,24 @@ class ParkingSpot
 public:
     char size;    // Size of the spot
     int occupied; // Indicates if the spot is occupied (1 for occupied, 0 for unoccupied)
+    int priority;
 
     ParkingSpot(char s, int o)
     {
         this->size = s;
         this->occupied = o;
+         if (s == 'S')
+        {
+            priority = 1;
+        }
+        else if (s == 'M')
+        {
+            priority = 2;
+        }
+        else if (s == 'L')
+        {
+            priority = 3;
+        }
     }
 };
 
@@ -101,6 +114,62 @@ public:
         return emptySpots;
     }
 
+// binary search wil be used to find first index of required size in each row and then find out each vacant index
+// Function to filter empty spots based on vehicle size using binary search in each row
+// Inside filterEmptySpotsBinarySearch function
+// Add debugging output to trace the binary search
+vector<array<int, 2> > filterEmptySpotsBinarySearch(char vehicleSize)
+    {
+        vector<array<int, 2> > emptySpots;
+
+        int vehiclePriority;
+        if (vehicleSize == 'S')
+        {
+            vehiclePriority = 1;
+        }
+        else if (vehicleSize == 'M')
+        {
+            vehiclePriority = 2;
+        }
+        else if (vehicleSize == 'L')
+        {
+            vehiclePriority = 3;
+        }
+        else
+        {
+            // Handle invalid size
+            return emptySpots;
+        }
+
+        for (int i = 0; i < rows; ++i)
+        {
+            int left = 0;
+            int right = columns - 1;
+
+            while (left <= right)
+            {
+                int mid = left + (right - left) / 2; // Calculate the middle column index
+
+                if (lot[i][mid]->occupied == 0 && lot[i][mid]->size == vehicleSize)
+                {
+                    array<int, 2> coords = {i, mid};
+                    emptySpots.push_back(coords);
+                    break; // Exit the while loop
+                }
+                else if (lot[i][mid]->priority < vehiclePriority)
+                {
+                    left = mid + 1; // Search the right half
+                }
+                else
+                {
+                    right = mid - 1; // Search the left half
+                }
+            }
+        }
+
+        return emptySpots;
+    }
+
     // Destructor to write occupancy status to a file
     ~ParkingLot()
     {
@@ -154,7 +223,7 @@ int main()
         cin >> vehicleSize;
 
         // Filter empty spots based on vehicle size
-        vector<array<int, 2> > emptySpots = parkingLot.filterEmptySpots(vehicleSize);
+        vector<array<int, 2> > emptySpots = parkingLot.filterEmptySpotsBinarySearch(vehicleSize);
 
         // Display the filtered empty spots
         cout << "Empty spots for your vehicle size:" << endl;
